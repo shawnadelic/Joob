53a204c131cfd0b1c04a76d10eb1a5ab185a64f0
shawnadelic <shawnhmh@gmail.com>
2015-07-09 00:39:17 -0700

    ORM relationships fixed

--- joob/rhyme_dictionary.py
+++ joob/rhyme_dictionary.py
@@ -1,20 +1,12 @@
-from nltk.corpus import cmudict as cmu
-from nltk.corpus import wordnet
 import os.path
 import random
-import sqlite3
-import json
 from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from collections import defaultdict
+from nltk.corpus import cmudict as cmu
 
 Base = declarative_base()
-
-#rhyme_table = Table('rhyme', Base.metadata,
-    #Column('left_word', String, ForeignKey('entry.word'), primary_key=True),
-    #Column('right_word', String, ForeignKey('entry.word'), primary_key=True)
-#)
 
 class_match_table = Table('class_match', Base.metadata,
     Column('rhyme_class_pron', String, ForeignKey('rhyme_class.pron'), primary_key=True),
@@ -23,19 +15,29 @@ class_match_table = Table('class_match', Base.metadata,
 class Entry(Base):
     __tablename__ = "entry"
     word = Column(String, primary_key=True)
-    #right_words = relationship("Entry", secondary=rhyme_table,
-        #primaryjoin=word==rhyme_table.c.left_word,
-        #secondaryjoin=word==rhyme_table.c.right_word,
-        #backref="left_words")
-    class_matches = relationship("Match", secondary=class_match_table,
+    class_matches = relationship("RhymeClass", secondary=class_match_table,
         backref="word_match")
+
+    def __init__(self, word):
+        self.word = word
+
+    def __repr__(self):
+        return self.word
 
 class RhymeClass(Base):
     __tablename__ = "rhyme_class"
     pron = Column(String, primary_key=True)
 
+    def __init__(self, pron):
+        self.pron = pron
+
+    def __repr__(self):
+        return self.pron
+
 class RhymeDictionary(object):
+
     def __init__(self, db_file, syllable_trim=2, match_trim=5):
+
         # Connect to database
         database_exists = os.path.isfile(db_file)
         engine = create_engine('sqlite:///' + db_file, echo=True)
@@ -48,17 +50,29 @@ class RhymeDictionary(object):
 
         # Create SessionMaker
         self.SessionMaker = sessionmaker(bind=engine)
+        session = self.SessionMaker()
 
         # Build dict from CMU entries
         cmu_dict = self.build_cmu_dict()
-        for item in cmu_dict.items()[:20]:
-            print item[0], item[1]
+        seen = []
+        for word, rhyme_classes in cmu_dict.items():
+            entry = Entry(word)
+            for rhyme_class in rhyme_classes:
+                rhyme_class = str(rhyme_class)
+                if rhyme_class not in seen:
+                    rhyme = RhymeClass(rhyme_class)
+                    session.add(rhyme)
+                    seen.append(rhyme_class)
+            session.add(entry)
+            session.commit()
+            session = self.SessionMaker()
             #print item, sorted(set(r for r in self.get_rhyme_classes(item[1])))
         # Idea for test - Verify comically results in two different pronunciations
         # Build pronunciation dict from CMU dict - Integrated into build_cmu_dict
         # Build matching syllables dict (rhyme dict)
         # Build matching syllables by word dict
         # Write to database
+        #session.commit()
 
     def build_cmu_dict(self):
         results = defaultdict(list)
