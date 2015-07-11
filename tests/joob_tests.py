5529db2bda3564f829d963584fbf4638908f2645
shawnadelic <shawnhmh@gmail.com>
2015-07-11 14:21:03 -0700

    Added test stubs

--- tests/joob_tests.py
+++ tests/joob_tests.py
@@ -8,3 +8,7 @@ class JoobClass(unittest.TestCase):
         self.assertEqual(self.rhyme_dict.a, "No")
     def test_one(self):
         self.assertEqual(self,self)
+
+# Test word with no other rhymes (beige)
+# Test word with numerous pronunciations (comically)
+# Test that words don't return themselves (jew)
