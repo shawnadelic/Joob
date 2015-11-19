c8c6b7e25eccf322603060cca010a308c9b33311
SAA <saa@SAA.(none)>
2015-11-19 15:59:37 -0800

    Finished integrating database functionality

--- joob/rhyme_dictionary.py
+++ joob/rhyme_dictionary.py
@@ -10,19 +10,14 @@
 
 # def build_database
 
-import os.path, imp, random, nltk
+import random, nltk, sys, os
 from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from collections import defaultdict
 
-if "hey" == "hey":
-    from nltk.corpus import cmudict as cmu
+# Setup ORM
 
-# Download correct corpus, if not already downloaded
-nltk.download('cmudict', quiet=True)
-
-# Define classes/objects for ORM
 Base = declarative_base()
 
 ClassMatchTable = Table('ClassMatch', Base.metadata,
@@ -47,127 +42,147 @@ class RhymeClass(Base):
     def __repr__(self):
         return "<RhymeClass(pron='%s')>" % self.pron
 
-class RhymeDictionary(object):
+def get_rhyme_classes(pron_list, syllable_trim):
+    rhyme_classes = []
+    for pron in pron_list:
+        for a in range(len(pron)):
+            rhyme_class = tuple(pron[a:])
+            if len(rhyme_class) > syllable_trim:
+                rhyme_classes.append(rhyme_class)
+    return rhyme_classes
 
-    def __init__(self, db_file, syllable_trim=1, match_trim=5):
+def build_database(db_file, syllable_trim=1, match_trim=5):
+    """
+    Build the database from CMU dictionary
+    """
 
-        # Set parameters and create dictionaries
-        self.syllable_trim = syllable_trim
-        self.rhyme_dict = dict()
-        self.reverse_rhyme_dict = defaultdict(list)
+    # Download correct corpus, if not already downloaded
+    nltk.download('cmudict', quiet=False)
+    cmu = nltk.corpus.cmudict
 
-        # Connect to database
-        database_exists = os.path.isfile(db_file)
-        engine = create_engine('sqlite:///' + db_file, echo=True)
-        engine.connect()
+    # Initialize dictionaries
+    rhyme_dict = dict()
+    reverse_rhyme_dict = defaultdict(list)
+    entry_dict = dict()
+    rhyme_class_dict = dict()
 
-        # Create SessionMaker
-        self.SessionMaker = sessionmaker(bind=engine)
+    # Connect to database
+    engine = create_engine('sqlite:///' + db_file, echo=True)
+    engine.connect()
 
-        # Check if database existed prior to connection, otherwise define
-        if database_exists: 
-            print "Opening database"
-            # Read from database
-        else:
-            print "Building database"
-            Base.metadata.create_all(engine)
-            #session = self.SessionMaker()
-            #entry = Entry(word="Jeb")
-            #session.add(entry)
-            #rhyme_class = RhymeClass(pron="Presidents")
-            #session.add(rhyme_class)
-            #entry.class_matches.append(rhyme_class)
-            #session.commit()
-            #session = self.SessionMaker()
-            #lookup = session.query(RhymeClass).filter_by(pron="Presidents").one()
-            #print lookup, lookup.word_matches
+    # Create SessionMaker
+    Session = sessionmaker(bind=engine)
 
-            print "Building rhyme dictionary..."
-            self.build_dictionaries()
-            # Build database 
+    print "Building database..."
+    Base.metadata.create_all(engine)
 
-        # Write to database
+    # Open session
+    session = Session()
 
-    # Build dictionaries from CMU entries
-    def build_dictionaries(self):
-        # Initialize dictionaries
-        rhyme_dict = dict()
-        reverse_rhyme_dict = defaultdict(list)
-        entry_dict = dict()
-        rhyme_class_dict = dict()
+    # Iterate through CMU dictionary and add entries to database
+    print "Adding words..."
+    for word, pron_list in cmu.dict().items():
+        entry = Entry(word=word)
+        entry_dict[word] = entry
+        session.add(entry)
 
-        # Open session
-        session = self.SessionMaker()
+        rhyme_classes = get_rhyme_classes(pron_list, syllable_trim)
+        rhyme_dict[word] = rhyme_classes
+        for rhyme_class in rhyme_classes:
+            if word not in reverse_rhyme_dict[rhyme_class]:
+                reverse_rhyme_dict[rhyme_class].append(word)
 
-        # Iterate through CMU dictionary and add entries to database
-        for word, pron_list in cmu.dict().items():
-            entry = Entry(word=word)
-            entry_dict[word] = entry
-            session.add(entry)
+    # Iterate through reverse_rhyme_dict, connecting rhyme class to words
+    print "Adding rhyme classes..."
+    for rhyme_class, word_list in reverse_rhyme_dict.items():
+        pron = " ".join(rhyme_class)
+        rhyme_class = RhymeClass(pron=pron)
+        rhyme_class_dict[pron] = rhyme_class.pron
+        session.add(rhyme_class)
 
-            rhyme_classes = self.get_rhyme_classes(pron_list)
-            rhyme_dict[word] = rhyme_classes
-            for rhyme_class in rhyme_classes:
-                if word not in reverse_rhyme_dict[rhyme_class]:
-                    reverse_rhyme_dict[rhyme_class].append(word)
+        for word in word_list:
+            entry_dict[word].class_matches.append(rhyme_class)
 
-        # Iterate through reverse_rhyme_dict, connecting rhyme class to words
-        for rhyme_class, word_list in reverse_rhyme_dict.items():
-            pron = " ".join(rhyme_class)
-            rhyme_class = RhymeClass(pron=pron)
-            rhyme_class_dict[pron] = rhyme_class
-            session.add(rhyme_class)
+    # Commit changes to database
+    session.commit()
+    
+    return Session
 
-            for word in word_list:
-                entry_dict[word].class_matches.append(rhyme_class)
+def connect_to_database(db_file):
+    # Connect to database
+    print "Connecting with database..."
+    engine = create_engine('sqlite:///' + db_file, echo=False)
+    engine.connect()
 
-        # Commit changes to database
-        session.commit()
+    # Create SessionMaker
+    Session = sessionmaker(bind=engine)
 
-    # Return all entries in reverse rhyme dictionary
-    def get_rhymes(self, word):
-        result = []
-        try:
-            rhyme_classes = sorted(self.rhyme_dict[word],
-                    key=lambda pron_list: len(pron_list))
-            for rhyme_class in rhyme_classes:
-                rhyme_list = self.reverse_rhyme_dict[rhyme_class]
-                if len(rhyme_list) > 1:
-                    result.append([rhyme_class, rhyme_list])
-        finally:
-            return result
-
-    def all_rhymes(self, word):
-        result = []
-        rhymes = self.get_rhymes(word)
-        print rhymes
-        for word, rhyme_list in rhymes:
-            result += rhyme_list
-        return result
-
-    # Return random rhyme for word
-    def random_rhyme(self, word):
-        all_rhymes = self.all_rhymes(word)
-        try:
-            return random.choice(all_rhymes)
-        except IndexError:
-            return None
-
-    # Generates all possible rhyme classes from pronuncation
-    def get_rhyme_classes(self, pron_list):
-        rhyme_classes = []
-        for pron in pron_list: 
-            for a in range(len(pron)):
-                rhyme_class = tuple(pron[a:])
-                if len(rhyme_class) > self.syllable_trim:
-                    rhyme_classes.append(rhyme_class)
-        return rhyme_classes
+    return Session
 
 if __name__ == "__main__":
-    rhyme_dict = RhymeDictionary(db_file="test.db")
+    db_file = sys.argv[1]
+
+    if not os.path.isfile(db_file):
+        Session = build_database(db_file)
+    else:
+        Session = connect_to_database(db_file)
+
+    session = Session()
+
     while True: 
+        # SELECT entry_word FROM ClassMatch WHERE rhyme_class_pron IN
+        #   (SELECT pron FROM RhymeClass INNER JOIN ClassMatch on RhymeClass.pron = ClassMatch.rhyme_class_pron WHERE ClassMatch.entry_word = 'testing');
         word = str(raw_input("Enter a word to test (or /quit to exit): "))
+        word_object = session.query(Entry).filter(Entry.word==word).first()
+        if word_object:
+            rhyme_classes = session.query(RhymeClass).filter(RhymeClass.word_matches.contains(word_object)).all()
+            for result in sorted(rhyme_classes, key=lambda rhyme: len(rhyme.pron)):
+                word_matches = session.query(Entry).filter(Entry.class_matches.contains(result))
+                print 
+                print result.pron
+                for word in word_matches:
+                    print word.word
         if word == "/quit":
             break
-        rhyme_dict.all_rhymes(word)
-        print rhyme_dict.random_rhyme(word)
+
+
+#     # Return all entries in reverse rhyme dictionary
+#     def get_rhymes(self, word):
+#         result = []
+#         try:
+#             rhyme_classes = sorted(self.rhyme_dict[word],
+#                     key=lambda pron_list: len(pron_list))
+#             for rhyme_class in rhyme_classes:
+#                 rhyme_list = self.reverse_rhyme_dict[rhyme_class]
+#                 if len(rhyme_list) > 1:
+#                     result.append([rhyme_class, rhyme_list])
+#         finally:
+#             return result
+# 
+#     def all_rhymes(self, word):
+#         result = []
+#         rhymes = self.get_rhymes(word)
+#         print rhymes
+#         for word, rhyme_list in rhymes:
+#             result += rhyme_list
+#         return result
+# 
+#     # Return random rhyme for word
+#     def random_rhyme(self, word):
+#         all_rhymes = self.all_rhymes(word)
+#         try:
+#             return random.choice(all_rhymes)
+#         except IndexError:
+#             return None
+# 
+#     # Generates all possible rhyme classes from pronuncation
+#     def get_rhyme_classes(self, pron_list):
+#         rhyme_classes = []
+#         for pron in pron_list: 
+#             for a in range(len(pron)):
+#                 rhyme_class = tuple(pron[a:])
+#                 if len(rhyme_class) > self.syllable_trim:
+#                     rhyme_classes.append(rhyme_class)
+#         return rhyme_classes
+# 
+# 
