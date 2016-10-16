e3806b6923027a269d50b8350f51fb98ac7ab2df
Shawn Her Many Horses <shawnhmh@gmail.com>
2016-10-16 12:53:12 -0700

    Use argparse for argument parsing

--- joob/commands.py
+++ joob/commands.py
@@ -1,6 +1,20 @@
-from joob.rhyme_dictionary import RhymeDictionary, build_database
+import argparse
+
+from joob.rhyme_dictionary import build_database
 
 
 def build_dict():
-    Session = build_database("test.db")
-    RhymeDictionary(Session, 0)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--db", help="Filename for sqlite3 database")
+    parser.add_argument("--trim",
+                        help="Trim level for generating rhyme phonemes",
+                        type=int)
+    args = parser.parse_args()
+
+    # Get arguments or set to defaults
+    database = args.db or "db.sqlite3"
+    trim = args.trim or 2
+
+    # Build database
+    print("Building database %s" % database)
+    build_database(database, trim)
